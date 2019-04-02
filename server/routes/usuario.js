c6c11298b954f727b3c2aa2162c6be3fc3322df1
abcd452 <davidgaona2@gmail.com>
2019-04-02 14:22:19 +0900

    'rutas_usuario'

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -8,8 +8,11 @@ const {verificaToken, verificaUserRole} = require('../midlewares/midlewares');
 app.get('/profile/:id',verificaToken,db.getUserById);
 app.get('/profile/dirfav/:id',verificaToken,db.getDirections);
 app.post('/profile/dirfav', db.createDirFav);
-app.post('/profile/pedirCarrera', db.pedirCarrera); //
+app.post('/profile/pedirCarrera', db.pedirCarrera);
 app.post('/profile/confirmarCarrera', db.confirmarCarrera);
+app.post('/profile/notificarCarreraTerminada', db.notificarCarreraTerminada);
+app.post('/profile/calificarTaxista', db.calificarTaxista);
+
 
 
 module.exports = app;
