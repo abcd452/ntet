aee363dfb664d7f42703ba94183d9744e4116816
Al3xCodeMaster <gonzalez.alexander@correounivalle.edu.co>
2019-04-15 12:55:03 -0500

    Se mueve todo a la carpeta Frontend

--- ntet/server/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const db = require('../queries/queries');
-
-app.post('/login/user',db.loginUser);
-app.post('/login/driver',db.loginTaxista);
-
-module.exports = app;
